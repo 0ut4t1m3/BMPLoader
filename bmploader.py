0a0ef450e0d14054cea35c10fc3df9114530cfb9
0ut4t1m3 <mikefoxme@gmail.com>
2024-07-30 18:04:16 +0930

    Update bmploader.py
    Made file header processing less confusing.
    Corrected a byte offset issue.

--- bmploader.py
+++ bmploader.py
@@ -14,8 +14,6 @@
 #You should have received a copy of the GNU General Public License
 #along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
-from struct import unpack
 import framebuf
 import gc
 
@@ -26,68 +24,64 @@ class BMPLoader:
         self.__sprheight = height
 
         with open(imgfile, 'rb') as f:
-            data = f.read(14)
-            BMP_id = data[0:2]
-            data = f.read(4)
-            DIB_len = unpack("<I", data[0:4])[0]
-            data = f.read(DIB_len - 4)
-            DIB_w = unpack("<I", data[0:4])[0]
-            DIB_h = unpack("<I", data[4:8])[0]
-            DIB_depth = unpack("<H", data[10:12])[0]
-            DIB_comp = unpack("<I", data[12:16])[0]
-            DIB_plt_num_info = unpack("<I", data[28:32])[0]
-            self.width  = DIB_w
-            self.height = DIB_h
+            b=bytearray(54)
+            b=f.read(54)
+            bmp_id = b[0:2]
+            self.width = b[18] + (b[19]<<8) + (b[20]<<16) +(b[21]<<24)
+            self.height = b[22] + (b[23]<<8) + (b[24]<<16) +(b[25]<<24)
+            bits_per_pixel = b[28] + (b[29]<<8)
+            compression = b[30] + (b[31]<<8) + (b[32]<<16) +(b[33]<<24)
+            total_cols = b[46] + (b[47]<<8) + (b[48]<<16) +(b[49]<<24)
             
             #check image is valid
-            assert BMP_id == b"BM", \
+            assert bmp_id == b"BM", \
                 "Not a valid BMP file"
-            assert DIB_comp == 0, \
+            assert compression == 0, \
                 "Compression is not supported"
-            assert 3 < DIB_depth < 9, \
-                "Only 16 and 265 colour bitmaps are supported"
+            assert 3 < bits_per_pixel < 9, \
+                f"Only 16 and 265 colour bitmaps are supported"
             if width:
-                assert width <= DIB_w, \
-                    f"Sprite width must be within image dimensions ({DIB_w})"
+                assert width <= self.width, \
+                    f"Sprite width must be within image dimensions ({self.width})"
             if height:
-                assert height <= DIB_h, \
-                    f"Sprite height must be within image dimensions ({DIB_h})"
+                assert height <= self.height, \
+                    f"Sprite height must be within image dimensions ({self.height})"
                 
-            if DIB_w % 8:
-                padding = 8 - DIB_w % 8
+            if self.width % 8:
+                padding = 8 - self.width % 8
             else:
                 padding = 0    
                 
             #set up palette
-            if DIB_depth == 4:
-                self.__palette = framebuf.FrameBuffer(bytearray(32), 16, 1, framebuf.RGB565)
+            if bits_per_pixel == 4:
+                palette = framebuf.FrameBuffer(bytearray(32), 16, 1, framebuf.RGB565)
             else:    
-                self.__palette = framebuf.FrameBuffer(bytearray(512), 256, 1, framebuf.RGB565)
-            for i in range(DIB_plt_num_info):
-                    data = f.read(4)
-                    self.__palette.pixel(i,0,self.rgb(data[2],data[1],data[0]))    
+                palette = framebuf.FrameBuffer(bytearray(512), 256, 1, framebuf.RGB565)
+            b = bytearray(4)
+            for i in range(total_cols):
+                b = f.read(4)
+                palette.pixel(i,0,self.rgb(b[2],b[1],b[0]))    
             
             #set up image buffer, width aligned to 8 bits
             in_mv  = memoryview(f.read())
-            out_mv = memoryview(bytearray(((DIB_w + padding) * DIB_h) // (2 if DIB_depth == 4 else 1)))
+            out_mv = memoryview(bytearray(((self.width + padding) * self.height) // (2 if bits_per_pixel == 4 else 1)))
             
             #bmp format has 0,0 at the bottom right so we need to mirror the y axis
-            bmplen = ((DIB_w + padding) * DIB_h) // (2 if DIB_depth == 4 else 1)
-            linewid = (DIB_w + padding) // (2 if DIB_depth == 4 else 1)
+            bmplen = ((self.width + padding) * self.height) // (2 if bits_per_pixel == 4 else 1)
+            linewid = (self.width + padding) // (2 if bits_per_pixel == 4 else 1)
             #this looks ugly but works, we use memoryview to slice the buffer into lines and remap
-            for y in range(DIB_h):
-                out_mv[bmplen-(y*linewid+linewid):bmplen-(y*linewid+1)] = in_mv[y*linewid:y*linewid+linewid-1]
+            for y in range(self.height):
+                out_mv[bmplen-(y*linewid+linewid):bmplen-(y*linewid)] = in_mv[y*linewid:y*linewid+linewid]
             
-            tempbuff = framebuf.FrameBuffer(out_mv, DIB_w + padding, DIB_h, framebuf.GS4_HMSB if DIB_depth == 4 else framebuf.GS8)
+            tempbuff = framebuf.FrameBuffer(out_mv, self.width + padding, self.height, framebuf.GS4_HMSB if bits_per_pixel == 4 else framebuf.GS8)
             
-            #initilise buffer to the proper frame size
-            self.__imgbuff = framebuf.FrameBuffer(bytearray(DIB_w * DIB_h * 2), DIB_w, DIB_h, framebuf.GS4_HMSB if DIB_depth == 4 else framebuf.GS8)
-            self.__imgbuff.blit(tempbuff,0,0,-1)
+            #initilise buffer to the proper frame size and colourspace
+            self.__imgbuff = framebuf.FrameBuffer(bytearray(self.width * self.height * 2), self.width, self.height, framebuf.RGB565)
+            self.__imgbuff.blit(tempbuff,0,0,-1,palette)
             
             #if we're using the sprite functions set up a buffer to handle the sprite
             if width:
-                self.__tile = framebuf.FrameBuffer(bytearray(width * (height if height else DIB_h) * 2), width, height if height else DIB_h, framebuf.GS4_HMSB if DIB_depth == 4 else framebuf.GS8)
-        
+                self.__tile = framebuf.FrameBuffer(bytearray(width * (height if height else self.height) * 2), width, height if height else self.height, framebuf.RGB565)
         gc.collect()
         
     #RGB565 conversion
@@ -96,17 +90,17 @@ class BMPLoader:
         
     #draw image to given framebuffer
     def draw(self,buffer,x=0,y=0,bg=-1):
-        buffer.blit(self.__imgbuff,x,y,bg,self.__palette)
+        buffer.blit(self.__imgbuff,x,y,bg)
         
     #draw image sprite in index mode
     def draw_index(self,buffer,x=0,y=0,index=0,bg=-1):
         assert self.__sprwidth, f"Index mode called without correct initilisation"
         self.__tile.blit(self.__imgbuff, -self.__sprwidth * index, 0)
-        buffer.blit(self.__tile,x,y,bg,self.__palette)
+        buffer.blit(self.__tile,x,y,bg)
         
     #draw image sprite in xy mode
     def draw_xy(self,buffer,x=0,y=0,crop_x=0,crop_y=0,bg=-1):
         assert self.__sprheight, f"XY mode called without correct initilisation"
         self.__tile.blit(self.__imgbuff, -crop_x, -crop_y)
-        buffer.blit(self.__tile,x,y,bg,self.__palette)
+        buffer.blit(self.__tile,x,y,bg)
 
